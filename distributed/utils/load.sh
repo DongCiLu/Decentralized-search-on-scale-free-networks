cb4c88a747a93eeee8f3b4d96ed004f860a15dec
DongCiLu <zlu12@vols.utk.edu>
2016-03-30 16:07:51 -0400

    incremental

--- distributed/utils/load.sh
+++ distributed/utils/load.sh
@@ -1,6 +1,6 @@
 echo "%CPU %MEM ARGS $(date)" > load.log
 while [ -e "load.log" ];
 do 
-    ps -e -o pcpu,pmem,args | grep "ds_dist" | grep -v "grep" | grep -v "load.sh" >> load.log;
+    ps -e -o pcpu,pmem,args | grep "ds_dist" | grep -v "grep\|load.sh\|mpiexec" >> load.log;
     sleep 0.1;
 done
