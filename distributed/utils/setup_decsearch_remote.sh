94b036cff85c28e30e29c2beffe7496f0292d9ad
DongCiLu <zlu12@vols.utk.edu>
2016-03-30 15:42:27 -0400

    incremental

---
+++ distributed/utils/setup_decsearch_remote.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get -y install libopenmpi-dev openmpi-bin default-jdk 
+sudo chown zlu12 /local
+
+/usr/bin/geni-get key > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+chmod 644 ~/.ssh/authorized_keys
