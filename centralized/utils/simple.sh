d32d173db847e3c3cd18cb13d1783cb510d25576
DongCiLu <zlu12@vols.utk.edu>
2016-04-24 16:14:32 -0400

    stepy mode for centralized version

--- centralized/utils/simple.sh
+++ centralized/utils/simple.sh
@@ -1,9 +1,9 @@
-./ds_cent datasets/wiki_wcc.txt &> log_wiki &
-./ds_cent datasets/skitter_wcc.txt &> log_skitter &
-./ds_cent datasets/baidu_wcc.txt &> log_baidu &
-./ds_cent datasets/Livejournal_wcc.txt &> log_Livejournal &
-./ds_cent datasets/hollywood_wcc.txt &> log_hollywood &
-./ds_cent datasets/orkut_wcc.txt &> log_orkut &
-./ds_cent datasets/sinaweibo_wcc.txt &> log_sinaweibo &
-./ds_cent datasets/webuk_wcc.txt &> log_webuk &
-./ds_cent datasets/friendster_wcc.txt &> log_friendster &
+./ds_cent datasets/wiki_wcc.txt 1 &> log_wiki &
+./ds_cent datasets/skitter_wcc.txt 1 &> log_skitter &
+./ds_cent datasets/baidu_wcc.txt 1 &> log_baidu &
+./ds_cent datasets/Livejournal_wcc.txt 1 &> log_Livejournal &
+./ds_cent datasets/hollywood_wcc.txt 1 &> log_hollywood &
+./ds_cent datasets/orkut_wcc.txt 1 &> log_orkut &
+./ds_cent datasets/sinaweibo_wcc.txt 1 &> log_sinaweibo &
+./ds_cent datasets/webuk_wcc.txt 1 &> log_webuk &
+./ds_cent datasets/friendster_wcc.txt 1 &> log_friendster &
