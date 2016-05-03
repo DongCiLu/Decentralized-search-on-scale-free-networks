7ba94990dd11e5010ea8b0dfc70b799d3169b951
DongCiLu <zlu12@vols.utk.edu>
2016-05-03 15:14:50 -0400

    add ec2

--- distributed/utils/setup_decsearch_ec2.sh
+++ distributed/utils/setup_decsearch_ec2.sh
@@ -1,27 +1,31 @@
-internal_ip_array=("10.142.0.2" "10.142.0.3")
+# internal_ip_array=("10.142.0.2" "10.142.0.3" "10.142.0.4" "10.142.0.5")
 
-sudo chmod 600 ~/DecSearch/distributed/utils/others/dc_ec2.pem
+mkdir /home/ubuntu/datasets
+sudo mount /dev/sdb1 /home/ubuntu/datasets
+sudo chmod 600 /home/ubuntu/DecSearch/distributed/utils/others/dc_ec2.pem
+sudo cp /home/ubuntu/DecSearch/distributed/utils/sshd_config /etc/ssh/sshd_config
+sudo service ssh restart
 
 ssh-keygen -t rsa -b 2048
-# prompts first
 for internal_ip in "${internal_ip_array[@]}"
 do
-    cat /home/ubuntu/.ssh/id_rsa.pub | ssh -i /home/ubuntu/DecSearch/distributed/utils/others/dc_ec2.pem ${internal_ip} "cat - >> /home/ubuntu/.ssh/authorized_key2"
+    cat /home/ubuntu/.ssh/id_rsa.pub | ssh -i /home/ubuntu/DecSearch/distributed/utils/others/dc_ec2.pem ${internal_ip} "cat - > /home/ubuntu/.ssh/authorized_keys2"
+    cat /home/ubuntu/.ssh/id_rsa | ssh ${internal_ip} "cat - > /home/ubuntu/.ssh/id_rsa"
+    ssh ${internal_ip} "sudo cat /home/ubuntu/.ssh/authorized_keys2 > /home/ubuntu/.ssh/id_rsa.pub"
+    ssh ${internal_ip} "sudo chmod 600 /home/ubuntu/.ssh/id_rsa"
+    ssh ${internal_ip} "sudo chmod 644 /home/ubuntu/.ssh/authorized_keys2"
 done
-ssh-copy-id zlu12@lanterns2.eecs.utk.edu
 
 for internal_ip in "${internal_ip_array[@]}"
 do
     ssh ${internal_ip} 'sudo apt-get update'
-    ssh ${internal_ip} 'sudo apt-get -y install libopenmpi-dev openmpi-bin default-jdk'
-    scp /local/DecSearch/distributed/utils/sshd_config ${internal_ip}:/home/ubuntu/
-    ssh ${internal_ip} 'sudo mv /home/ubuntu/sshd_config /etc/ssh/'
+    ssh ${internal_ip} 'sudo apt-get -y install libopenmpi-dev openmpi-bin default-jdk zlib1g-dev'
 done
 
 sudo apt-get update
-sudo apt-get -y install vim ssh gcc g++ build-essential cmake zlib1g-dev git automake  
+sudo apt-get -y install vim ssh gcc g++ build-essential cmake git automake  
 
-cd /home/ubuntu
+cd /home/ubuntu 
 git clone https://github.com/dato-code/PowerGraph.git
 cd /home/ubuntu/PowerGraph/apps
 mkdir ds_dist
@@ -41,8 +45,7 @@ cd /home/ubuntu/PowerGraph
 cp /home/ubuntu/DecSearch/distributed/utils/boost_1_53_0.tar.gz /home/ubuntu/PowerGraph/deps/boost/src/
 cp /home/ubuntu/DecSearch/distributed/utils/libevent-2.0.18-stable.tar.gz /home/ubuntu/PowerGraph/deps/event/src/
 cd /home/ubuntu/PowerGraph/release/apps/ds_dist/
-# make -j4
-make
+make -j4
 
 ln -s /home/ubuntu/DecSearch/distributed /home/ubuntu/PowerGraph/release/apps/ds_dist/src
 ln -s /home/ubuntu/datasets /home/ubuntu/PowerGraph/release/apps/ds_dist/datasets
@@ -51,6 +54,7 @@ ln -s /home/ubuntu/DecSearch/distributed/utils/simple_ec2.sh /home/ubuntu/PowerG
 mkdir binary
 mv ds_dist binary/ds_dist_test
 
+rm /home/ubuntu/machines
 for internal_ip in "${internal_ip_array[@]}"
 do
     echo ${internal_ip} >> /home/ubuntu/machines
