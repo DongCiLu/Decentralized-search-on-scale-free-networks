abfa2cae74a18adb381044f4e9087037fc3284b7
zlu12@vols.utk.edu <zlu12@vols.utk.edu>
2017-11-20 07:37:30 -0500

    archive old tex

--- tex/cikm_tex/figures/scripts/old/plot_accuracy_dec.m
+++
@@ -1,8 +0,0 @@
-k_all=x;
-h=bar(k_all);
-set(gca,'Xlim',[0 10],'XTick',1:1:9,'XTickLabel',{'Wiki' 'Skitter' 'Baidu' 'Livejournal' 'hollywood' 'orkut' 'sinaweibo' 'webuk' 'friendster'});
-set(gca, 'Fontname', 'Times New Roman', 'Fontsize', 15);
-ylabel_hand=ylabel('Avg. errorr ratio');
-set(ylabel_hand,'Fontname', 'Times New Roman', 'Fontsize', 15);
-legend_hand = legend('TreeSketch','Dec. Search','Tie breaker DS','Orientation','horizontal');
-set(legend_hand,'Fontname', 'Times New Roman', 'Fontsize', 15);
--- tex/sigmod_tex/figures/scripts/plot_oh.m
+++
@@ -1,9 +0,0 @@
-k_all=x;
-h=bar(k_all);
-%set(gca,'Xlim',[0 10],'XTick',1:1:9,'XTickLabel',{'Google' 'Wiki' 'Skitter' 'Gene' 'Baidu' 'Facebook' 'Livejournal' 'hollywood' 'friendster'});
-set(gca, 'YScale', 'log');
-set(gca, 'Fontname', 'Times New Roman', 'Fontsize', 11);
-ylabel_hand=ylabel('Searching overhead [\mus]');
-set(ylabel_hand,'Fontname', 'Times New Roman', 'Fontsize', 13);
-legend_hand = legend('DS wo/ early termination','DS w/ early termination', 'Bidirectional DS', 'Tie break DS');
-set(legend_hand,'Fontname', 'Times New Roman', 'Fontsize', 13);
