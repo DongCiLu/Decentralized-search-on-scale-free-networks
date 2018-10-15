c1dff2259d2016787c68c6064be4ce6a6e2098e5
Zheng Lu <zlu12@vols.utk.edu>
2018-10-14 19:19:35 -0500

    remove old tex

--- tex/archived_tex/qual_tex/figures/scripts/plot_accuracy_dec.m
+++
@@ -1,8 +0,0 @@
-k_all=x;
-h=bar(k_all);
-set(gca,'Xlim',[0 10],'XTick',1:1:9,'XTickLabel',{'Google' 'Wiki' 'Skitter' 'Gene' 'Baidu' 'Facebook' 'Livejournal' 'hollywood' 'friendster'});
-set(gca, 'Fontname', 'Times New Roman', 'Fontsize', 15);
-ylabel_hand=ylabel('Avg. errorr ratio');
-set(ylabel_hand,'Fontname', 'Times New Roman', 'Fontsize', 15);
-legend_hand = legend('LCA distance','Dec. Search','Bidirectional DS','Tie breaker DS','All opt on','10x landmarks','Orientation','horizontal');
-set(legend_hand,'Fontname', 'Times New Roman', 'Fontsize', 15);
