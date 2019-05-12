e8c62711f9446b68bca05b3b2758390c1ba596a8
zlu12 <zlu12@node-0.zlu12-qv52207.graphengine-pg0.clemson.cloudlab.us>
2019-05-11 20:36:03 -0400

    minor change

--- centralized/utils/simple.sh
+++ centralized/utils/simple.sh
@@ -1,9 +1,11 @@
-./ds_cent datasets/wiki_wcc.txt 1 &> log_wiki &
-./ds_cent datasets/skitter_wcc.txt 1 &> log_skitter &
-./ds_cent datasets/baidu_wcc.txt 1 &> log_baidu &
-./ds_cent datasets/Livejournal_wcc.txt 1 &> log_Livejournal &
-./ds_cent datasets/hollywood_wcc.txt 1 &> log_hollywood &
-./ds_cent datasets/orkut_wcc.txt 1 &> log_orkut &
-./ds_cent datasets/sinaweibo_wcc.txt 1 &> log_sinaweibo &
-./ds_cent datasets/webuk_wcc.txt 1 &> log_webuk &
-./ds_cent datasets/friendster_wcc.txt 1 &> log_friendster &
+./testcase datasets/webuk_wccnse.txt webuk_testcases.txt &> log_webuk &
+./testcase datasets/friendster_wccnse.txt friendster_testcases.txt &> log_friendster &
+
+# ./ds_cent datasets/wiki_wcc.txt 1 &> log_wiki &
+# ./ds_cent datasets/skitter_wcc.txt 1 &> log_skitter &
+# ./ds_cent datasets/Livejournal_wcc.txt 1 &> log_Livejournal &
+# ./ds_cent datasets/hollywood_wcc.txt 1 &> log_hollywood &
+# ./ds_cent datasets/orkut_wcc.txt 1 &> log_orkut &
+# ./ds_cent datasets/sinaweibo_wcc.txt 1 &> log_sinaweibo &
+# ./ds_cent datasets/webuk_wcc.txt 1 &> log_webuk &
+# ./ds_cent datasets/friendster_wcc.txt 1 &> log_friendster &
